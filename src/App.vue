42a8ec18bcf89c915d251c10433fe1855eece2ae
Dhruv Seta <setadhruv56@gmail.com>
2019-07-07 16:18:44 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app" class="div">
+    <!-- using v-text <h1 v-text="name"></h1>
+    using v-html <h1 v-html="name"></h1>
+    using interpolation <h1>{{name}}</h1> -->
+
+    <homework></homework>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Homework from './components/Homework.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Homework
+  },
+  data() {
+    return {
+      name: 'Dhruv Seta',
+      list: [
+        {
+          content: 'First',
+          urlId: 55
+        },
+        {
+          content: 'second',
+          urlId: 65
+        },
+        {
+          content: 'third',
+          urlId: 75
+        }
+      ]
+    }
+  },
+  method: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Homework.vue
@@ -0,0 +1,66 @@
+<template>
+    <div  :class="{available: available}">
+        <h1 @click="changeCss" v-if="available">{{message}}</h1>
+        <h1 @click="changeCss" v-else>{{text2}}</h1>
+        <p>Count: {{count}}
+            <button @click="resetCount">Reset Count</button>
+        </p>
+        <p>You pressed reset after: {{before}} clicks</p>
+
+        <p @click="changeText">Testing 1-way Binding here: {{test}}</p>
+
+        <input type="text" v-model="live">
+        <p>{{live}}</p>
+    </div>
+</template> 
+
+<script>
+export default {
+    name: 'Homework',
+    data() {
+        return {
+            available: false,
+            test: 'Dhruv Seta',
+            message: "Now I'm green",
+            text2: "I'm red",
+            count: 0,
+            before: 0,
+            live: ''
+        }
+    },
+    methods: {
+        changeCss: function() {
+            this.available = !this.available;
+            this.count++;
+        },
+
+        changeText: function() {
+            this.test = 'YO WTF DONT TOUCH ME'
+        },
+
+        resetCount: function() {
+            console.log(this.count);
+            this.before = this.count;
+            this.count = 0;
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* h1 {
+    text-align: center;
+    background-color: rgba(17, 196, 196, 0.5);
+    color: rgb(15, 13, 109);
+} */
+
+h1 {
+    background-color: red;
+    color: white;
+}
+
+.available h1 {
+    background-color: green;
+}
+</style>
+
